efa5cd268ebc3182a29f948a2817f0b1a8c39ffb
noaheli <noah.eli@gmail.com>
2014-10-16 14:56:07 -0500

    First Comet you

--- 70-100pt.py
+++ 70-100pt.py
@@ -14,3 +14,15 @@
 
 # Minus 5pts if your code has no comments
 # Minus 10pts if you only commit once to github
+from Tkinter import *
+root = Tk()
+# Create the canvas widget
+drawpad = Canvas(root, width=800,height=600, background='brown')
+drawpad.grid(row=0, column=1)
+# create_oval(x,y,width,height,fill color)
+oval = drawpad.create_oval(10, 50, 100, 100, fill='green')
+#create_square(top left x,top left y, bottom right x, bottom right y, fill color)
+square = drawpad.create_rectangle(200,200,250,250, fill='red')
+#create_line(top left x,top left y, bottom right x, bottom right y, fill color)
+line = drawpad.create_line(0, 0, 200, 100)
+root.mainloop()
